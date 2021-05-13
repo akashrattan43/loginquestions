8b8e6df088adb84cb9b23644738d35676e1b2072
akash rattan <akashrattan43@Gmail.com>
2021-05-13 10:24:14 -0700

    data

--- src/data.js
+++ src/data.js
@@ -7,7 +7,7 @@ const questions = [
     },
     {
       id: 2,
-      title: 'How do I change my My Page password?',
+      title: 'How do I change my MyVerse password?',
       info:
         'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
     },
