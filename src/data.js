3a090e662d59fc9314ceffcd6d5d9c7962583e64
akash rattan <akashrattan43@Gmail.com>
2021-05-12 20:52:03 -0700

    done

---
+++ src/data.js
@@ -0,0 +1,34 @@
+const questions = [
+    {
+      id: 1,
+      title: 'Do I have to allow the use of cookies?',
+      info:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    },
+    {
+      id: 2,
+      title: 'How do I change my My Page password?',
+      info:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    },
+    {
+      id: 3,
+      title: 'What is ID?',
+      info:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    },
+    {
+      id: 4,
+      title: 'Whose birth number can I use?',
+      info:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    },
+    {
+      id: 5,
+      title: 'How do I receive my password?',
+      info:
+        'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.',
+    },
+  ]
+  export default questions
+  
